db9a2731cebf289ef09262fc3bce912acfa257dc
ccd <ccd@ccddeMacBook-Pro-2.local>
2016-04-13 10:40:07 +0800

    Initial commit

---
+++ app/src/main/java/com/demo/ccd/demotest_xnt/DemoTest_XNT.java
@@ -0,0 +1,13 @@
+package com.demo.ccd.demotest_xnt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DemoTest_XNT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_test__xnt);
+    }
+}
